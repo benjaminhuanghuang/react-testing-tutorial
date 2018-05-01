d06ced75c3b38c527dcbcba6aa09751a69cb19cd
Benjamin Huang <bhuang@rms.com>
2018-05-01 16:36:32 -0700

    add sinon mock api

--- _notes/react-ut-tools.md
+++ _notes/react-ut-tools.md
@@ -1,13 +1,29 @@
 
 ## Setup
-- Enzyme is used for React unit testing
+- Enzyme is used to test React component
+    elements rendered
+    props passed to component
+    simulate event like click
+    state changes
+
 ```
     npm install --save-dev enzyme
     npm install --save-dev enzyme-adapter-react-16
 ```
 
-    + Shallow is the simplest form of rendering a component with Enzyme. It only renders the component but not the content of components which are children to this component. It makes it possible to test the component in isolation. 
+    + shallow() is the simplest form of rendering a component with Enzyme. It only renders the component but not the content of components which are children to this component. It makes it possible to test the component in isolation. Be used for component tests in isolation (unit tests, lightweight integration).
 
+    + mount() renders all child components as well. It renders the whole component hierarchy. 
+    It is used for real integration tests.
+
+    + render() is similar to mount(), it renders all child components. But from a performance point of view, it is less expensive than mount(), because it doesn’t run the lifecycle methods of the component. So if you need access to child components but are not interested in lifecycle methods, you can use render() instead of mount().
+
+    If you would want to use more integration tests than unit tests, you would go for mount() or render() instead of shallow() in your component tests. 
+
+- sinno can be used for spies, stubs, and mocks. For example: asynchronous data fetching. 
+```
+    npm install --save-dev sinon
+```
 
 - Jsdom can create this artificial browser environment for React components.
 ```
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+
 import Counter from './components/Counter';
-import App, {doIncrement, doDecrement} from './App';
+import App, { doIncrement, doDecrement } from './App';
 
 
 describe('Local State', () => {
@@ -21,6 +23,18 @@ describe('Local State', () => {
 
 
 describe('App Component', () => {
+    const result = [3, 5, 9];
+    const promise = Promise.resolve(result);
+
+    before(() => {
+        // mocking the returned data from the third-party API request.
+        sinon.stub(axios, 'get').withArgs('http://mydomain/counter').returns(promise);
+    });
+
+    after(() => {
+
+    });
+
     it('renders the Counter wrapper', () => {
         const wrapper = shallow(<App />);
         expect(wrapper.find(Counter)).to.have.length(1);
@@ -37,4 +51,39 @@ describe('App Component', () => {
         counterWrapper = wrapper.find(Counter);
         expect(counterWrapper.props().counter).to.equal(-1);
     });
+
+    it('increments the counter', () => {
+        const wrapper = shallow(<App />);
+
+        wrapper.setState({ counter: 0 });
+        wrapper.find('button').at(0).simulate('click');
+
+        expect(wrapper.state().counter).to.equal(1);
+    });
+
+    it('decrements the counter', () => {
+        const wrapper = shallow(<App />);
+
+        wrapper.setState({ counter: 0 });
+        wrapper.find('button').at(1).simulate('click');
+
+        expect(wrapper.state().counter).to.equal(-1);
+    });
+
+    it('calls componentDidMount', () => {
+        sinon.spy(App.prototype, 'componentDidMount');
+
+        const wrapper = mount(<App />);  // test lifecycle 
+        expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+    });
+
+    // it('fetches async counters', () => {
+    //     const wrapper = shallow(<App />);
+
+    //     expect(wrapper.state().asyncCounters).to.equal(null);
+
+    //     promise.then(() => {
+    //         expect(wrapper.state().asyncCounters).to.equal(result);
+    //     });
+    // });
 });
